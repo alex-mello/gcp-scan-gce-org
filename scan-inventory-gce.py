d85dcf9b46477ae9ada92855a0385bd15278031e
Alex Mello <39780604+alex-mello@users.noreply.github.com>
2023-08-28 17:46:06 -0300

    Update scan-inventory-gce.py

--- scan-inventory-gce.py
+++ scan-inventory-gce.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+
 from googleapiclient import discovery
 
 import google.auth
@@ -27,6 +28,7 @@ if (len(sys.argv)) >=3:
     os = re.search(r'(.*)/(.*)', osu).group(2)
 
     # Get disk size/count
+    #dsize = str(response['disks'][0]['diskSizeGb']) + ' GB'
     num_disks = len(response['disks'])
 
     # Use machine type to get cpu count & memory size
@@ -44,6 +46,14 @@ if (len(sys.argv)) >=3:
     # Get status
     status = response['status']
 
+    # Check if the instance is running
+    if status == 'RUNNING':
+        publicip_status = "IP externo desativado"
+        publicip = ""
+    else:
+        publicip_status = "VM desligada"
+        publicip = ""
+
     # Get network Interfaces list of dictionaries
     ni = response['networkInterfaces']
 
@@ -51,8 +61,12 @@ if (len(sys.argv)) >=3:
     for d in ni:
         privateip = str(d['networkIP'])
         if 'accessConfigs' in d:
-            publicip = str(d['accessConfigs'][0]['natIP'])
+            if 'natIP' in d['accessConfigs'][0]:
+                publicip = str(d['accessConfigs'][0]['natIP'])
+                publicip_status = "IP reservado"
+            else:
+                publicip = "IP temporario"
         else:
             publicip = "NA"
 
-    print(f'{project},{instance},{zone},{mtype},{status},{os},{cpu},{memory},{num_disks},{privateip},{publicip}')
+    print(f'{project},{instance},{zone},{mtype},{status},{os},{cpu},{memory},{num_disks},{privateip},{publicip_status},{publicip}')
