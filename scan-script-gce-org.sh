a0d7db36679ffb3ef93ccd80a60fcece67eb0b04
Alex Mello <39780604+alex-mello@users.noreply.github.com>
2024-01-11 13:19:06 -0300

    Update scan-script-gce-org.sh

--- scan-script-gce-org.sh
+++ scan-script-gce-org.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
+# Log file path
+LOG_FILE="script_log.txt"
+
+# Function to log messages with timestamp
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" >> "$LOG_FILE"
+}
+
+# Header for the CSV file
 echo "PROJECT ID, NAME, ZONE, TYPE MACHINE, STATUS, SO, CPU, MEMORY, DISC, PRIVATE IP, STATUS_IP, IP PUBLICO" > gce-inventory_org.csv
+
+# Array of GCP project IDs
 prjs=( $(gcloud projects list | tail -n +2 | awk {'print $1'}) )
+
+# Loop through each project
 for i in "${prjs[@]}"
-    do
-        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> list.txt
-        echo "Setting Project: $i" >> list.txt
-        echo $(gcloud config set project $i)
-        if [[ $(gcloud services list --enabled --filter="NAME:compute.googleapis.com" --format="value(NAME)" | grep compute) ]]; then
-            echo "Compute API is enabled is in project - $i"
-            echo $(gcloud compute instances list | awk '{print $1,$2}' | tail -n +2| while read line; do echo "$i $line"; done |xargs -n3 sh -c 'python3  scan-inventory-gce.py $1 $2 $3 >> gce-inventory_org.csv' sh)
-        else
-            echo "Compute API is not enabled in project - $i"
-        fi       
-    done
+do
+    # Set the default quota project
+    log "Setting default quota project: $i"
+    gcloud auth application-default set-quota-project $i >> "$LOG_FILE"
+
+    # Log setting the current project
+    log "Setting Project: $i"
+    echo $(gcloud config set project $i) >> "$LOG_FILE"
+
+    # Check if Compute Engine API is enabled
+    if [[ $(gcloud services list --enabled --filter="NAME:compute.googleapis.com" --format="value(NAME)" | grep compute) ]]; then
+        # Log that Compute API is enabled in the current project
+        log "Compute API is enabled in project - $i"
+
+        # Get information about instances and append to the CSV file
+        echo $(gcloud compute instances list | awk '{print $1,$2}' | tail -n +2 | while read line; do echo "$i $line"; done | xargs -n3 sh -c 'python3  scan-inventory-gce.py $1 $2 $3 >> gce-inventory_org.csv' sh) >> "$LOG_FILE"
+    else
+        # Log that Compute API is not enabled in the current project
+        log "Compute API is not enabled in project - $i"
+    fi       
+done
